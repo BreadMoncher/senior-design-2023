8516660c2d0dcf90b63f92e770f15d902195c5cf
BreadMoncher <aidan0143@gmail.com>
2023-11-09 15:34:38 -0600

    some changes to main

--- src/main.py
+++ src/main.py
@@ -5,26 +5,70 @@ import time
 
 class FSMState(Enum):
     INITIALIZE = 1
-    FLIGHT_LOOP = 2
-    EXIT = 3
+    REFRESH_DATA = 2
+    RUN_COMMANDS = 3
+    FLIGHT_LOOP = 4
+    END_FLIGHT = 5
+    EXIT = 6
+
+system_state = {
+    "altitude": 0,
+    "obstacle_distance_front": 0,
+    "obstacle_distance_left": 0,
+    "obstacle_distance_right": 0,
+    "forward_velocity": 0,
+}
 
 def fsm():
-    global state
+    global state, system_state
 
     if state == FSMState.INITIALIZE:
         time.sleep(0.2)
-        state = FSMState.FLIGHT_LOOP
+        state = FSMState.REFRESH_DATA
+
+    if state == FSMState.REFRESH_DATA:
+        
+        # get teraranger data
+        if (not interface.teraranger_data.empty()):
+            while interface.teraranger_data.empty() != True:
+                data = interface.teraranger_data.get()
+
+            system_state["obstacle_distance_left"] = data[0]
+            system_state["obstacle_distance_front"] = data[1]
+            system_state["obstacle_distance_right"] = data[2]
+
+            print("teraranger:",data)
+            
+        else:
+            print("no teraranger data, continuing")
+            
+    if state == FSMState.RUN_COMMANDS:
+        # Get commands from the ground telemetry
+        while (not interface.telemetry_received_data.empty()):
+            next_command = interface.telemetry_received_data.get()
+
+            if (next_command == "takeoff"):
+                #takeoff
+                pass
+            elif (next_command == "land"):
+                #land
+                pass
+            
 
     if state == FSMState.FLIGHT_LOOP:
         
-        if interface.teraranger_data.empty():
-            print("no teraranger data, continuing")
-        else:
-            while interface.teraranger_data.empty() != True:
-                data = interface.teraranger_data.get()
-            print("teraranger:",data)
         
-        state = FSMState.INITIALIZE
+        
+        state = FSMState.REFRESH_DATA
+
+    if state == FSMState.END_FLIGHT:
+        # Land the drone safely
+        
+        state = FSMState.EXIT
+
+    if state == FSMState.EXIT:
+        # Exit the program
+        exit()
     
 
 def setup():
